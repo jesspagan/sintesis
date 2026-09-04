e97e29a9de7a41d993b1a1f00ad9bf3982d068e2
Jessica Pagan <3449831+jesspagan@users.noreply.github.com>
2026-09-04 19:08:51 -0400

    feat: add Roboto Flex font and enhance theme styling in App component

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,11 +1,32 @@
+import React, { useCallback, useEffect } from "react";
 import "./App.css";
 import { Button } from "./ui/button";
 
+type Theme = "CNN" | "decathlon";
+
 function App() {
+  const [theme, setTheme] = React.useState<Theme>("CNN");
+
+  const handleClick = useCallback(() => {
+    setTheme((prevTheme) => (prevTheme === "CNN" ? "decathlon" : "CNN"));
+    const root = document.documentElement;
+    root.setAttribute("data-theme", theme === "CNN" ? "CNN" : "decathlon");
+  }, [theme]);
+
+  useEffect(() => {
+    const root = document.documentElement;
+    root.setAttribute("data-theme", theme === "CNN" ? "CNN" : "decathlon");
+  }, [theme]);
+
   return (
     <div className="app">
-      <Button className="brand">botón</Button>
-      <Button className="neutral">botón</Button>
+      <Button className="default" onClick={handleClick}>
+        {theme}
+      </Button>
+      <div className="content">
+        <Button className="brand">botón marca</Button>
+        <Button className="neutral">botón neutral</Button>
+      </div>
     </div>
   );
 }
