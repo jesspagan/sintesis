2150212501a40480247574f7ea046871f4b145c8
Jessica Pagan <3449831+jesspagan@users.noreply.github.com>
2026-09-04 23:33:46 -0400

    feat: add Inter font and enhance theme styling with new variables and button states

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -2,20 +2,32 @@ import React, { useCallback, useEffect } from "react";
 import "./App.css";
 import { Button } from "./ui/button";
 
-type Theme = "CNN" | "decathlon";
+type Theme = "CNN" | "decathlon" | "figma";
 
 function App() {
   const [theme, setTheme] = React.useState<Theme>("CNN");
 
   const handleClick = useCallback(() => {
-    setTheme((prevTheme) => (prevTheme === "CNN" ? "decathlon" : "CNN"));
+    setTheme((prevTheme) =>
+      prevTheme === "CNN"
+        ? "decathlon"
+        : prevTheme === "decathlon"
+          ? "figma"
+          : "CNN",
+    );
     const root = document.documentElement;
-    root.setAttribute("data-theme", theme === "CNN" ? "CNN" : "decathlon");
+    root.setAttribute(
+      "data-theme",
+      theme === "CNN" ? "decathlon" : theme === "decathlon" ? "figma" : "CNN",
+    );
   }, [theme]);
 
   useEffect(() => {
     const root = document.documentElement;
-    root.setAttribute("data-theme", theme === "CNN" ? "CNN" : "decathlon");
+    root.setAttribute(
+      "data-theme",
+      theme === "CNN" ? "CNN" : theme === "decathlon" ? "decathlon" : "figma",
+    );
   }, [theme]);
 
   return (
